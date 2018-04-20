ce07e1242dc14ace668e483c18d2830b621a4726
John Deng <john.deng@qq.com>
2018-04-20 18:46:52 +0800

    support builder.Init and builder.Save

--- pkg/system/builder_test.go
+++ pkg/system/builder_test.go
@@ -4,19 +4,21 @@ import (
 	"testing"
 	"github.com/hidevopsio/hiboot/pkg/utils"
 	"github.com/magiconair/properties/assert"
+	"path/filepath"
+	"os"
 )
 
 const (
 	application = "application"
-	config      = "/config"
-	yaml        = "yaml"
+	config      = "config"
 )
 
 func TestBuilder(t *testing.T) {
+
 	b := &Builder{
-		Path:       utils.GetWorkingDir("") + config,
+		Path:       filepath.Join(utils.GetWorkingDir(""), "config"),
 		Name:       application,
-		FileType:   yaml,
+		FileType:   "yaml",
 		Profile:    "local",
 		ConfigType: Configuration{},
 	}
@@ -26,4 +28,41 @@ func TestBuilder(t *testing.T) {
 
 	c := cp.(*Configuration)
 	assert.Equal(t, "hiboot", c.App.Name)
+}
+
+
+func TestBuilderInit(t *testing.T) {
+	b := &Builder{
+		Path:       filepath.Join(os.TempDir(), "config"),
+		Name:       "foo",
+		FileType:   "yaml",
+		ConfigType: Configuration{},
+	}
+
+	err := b.Init()
+	assert.Equal(t, nil, err)
+}
+
+func TestBuilderSave(t *testing.T) {
+	b := &Builder{
+		Path:       filepath.Join(os.TempDir(), "config"),
+		Name:       "foo",
+		FileType:   "yaml",
+		ConfigType: Configuration{},
+	}
+
+	err := b.Init()
+	assert.Equal(t, nil, err)
+
+	c := &Configuration{
+		App: App{
+			Name: "foo",
+			Project: "bar",
+		},
+		Server: Server{
+			Port: 8080,
+		},
+	}
+	err = b.Save(c)
+	assert.Equal(t, nil, err)
 }
