3bbfc7b088002aa7a0df677bc6283924209edba1
John Deng <john.deng@qq.com>
2018-06-29 11:58:51 +0800

    update iris to v10.6.5

--- pkg/starter/web/application.go
+++ pkg/starter/web/application.go
@@ -31,6 +31,7 @@ import (
 	"github.com/hidevopsio/hiboot/pkg/utils"
 	"github.com/hidevopsio/hiboot/pkg/log"
 	"github.com/hidevopsio/hiboot/pkg/starter/db"
+	"github.com/kataras/iris/middleware/logger"
 )
 
 const (
@@ -171,6 +172,31 @@ func (wa *Application) Init(controllers ...interface{}) error {
 	}
 
 	wa.app = iris.New()
+
+	customLogger := logger.New(logger.Config{
+		// Status displays status code
+		Status: true,
+		// IP displays request's remote address
+		IP: true,
+		// Method displays the http method
+		Method: true,
+		// Path displays the request path
+		Path: true,
+		// Query appends the url query to the Path.
+		//Query: true,
+
+		//Columns: true,
+
+		// if !empty then its contents derives from `ctx.Values().Get("logger_message")
+		// will be added to the logs.
+		MessageContextKeys: []string{"logger_message"},
+
+		// if !empty then its contents derives from `ctx.GetHeader("User-Agent")
+		MessageHeaderKeys: []string{"User-Agent"},
+	})
+
+	wa.app.Use(customLogger)
+
 
 	// The only one Required:
 	// here is how you define how your own context will
