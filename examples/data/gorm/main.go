9de6461ceb022e3c1967a6b23fa24df9c7bf21b5
John Deng <john.deng@qq.com>
2018-09-11 08:33:12 +0800

    code clean up

--- examples/data/gorm/main.go
+++ examples/data/gorm/main.go
@@ -18,6 +18,8 @@ import (
 	_ "github.com/hidevopsio/hiboot/examples/data/gorm/controller"
 	"github.com/hidevopsio/hiboot/pkg/app/web"
 	_ "github.com/hidevopsio/hiboot/pkg/starter/actuator"
+	_ "github.com/hidevopsio/hiboot/pkg/starter/locale"
+	_ "github.com/hidevopsio/hiboot/pkg/starter/logging"
 )
 
 func main() {
