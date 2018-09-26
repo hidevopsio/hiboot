f3bb62f8f66d5681ff64548598ea0afc190d9aae
John Deng <john.deng@qq.com>
2018-09-26 20:26:18 +0800

    refactored idgen, use mac address to generate id instead of ip address

--- pkg/utils/idgen/idgen_test.go
+++ pkg/utils/idgen/idgen_test.go
@@ -1,6 +1,7 @@
 package idgen
 
 import (
+	"github.com/hidevopsio/hiboot/pkg/log"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -10,12 +11,14 @@ func TestNext(t *testing.T) {
 	t.Run("should generate id in uint", func(t *testing.T) {
 		id, err := Next()
 		assert.Equal(t, nil, err)
+		log.Info(id)
 		assert.NotEqual(t, 0, id)
 	})
 
 	t.Run("should generate id in string", func(t *testing.T) {
 		id, err := NextString()
 		assert.Equal(t, nil, err)
+		log.Info(id)
 		assert.NotEqual(t, 0, id)
 	})
 
