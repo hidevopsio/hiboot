91fcad605c06e8135bcfc1d918abd5ec11f58187
John Deng <john.deng@qq.com>
2019-08-06 15:59:29 +0800

    added ping pong callback to websocket

--- examples/web/jwt/service/counthandler.go
+++ examples/web/jwt/service/counthandler.go
@@ -42,3 +42,15 @@ func (h *CountHandler) OnMessage(data []byte) {
 func (h *CountHandler) OnDisconnect() {
 	log.Debugf("Connection with ID: %v has been disconnected!", h.connection.ID())
 }
+
+
+// OnPing is the websocket ping handler
+func (h *CountHandler) OnPing() {
+	log.Debugf("Connection with ID: %v has been pinged!", h.connection.ID())
+}
+
+
+// OnPong is the websocket pong handler
+func (h *CountHandler) OnPong() {
+	log.Debugf("Connection with ID: %v has been ponged!", h.connection.ID())
+}
